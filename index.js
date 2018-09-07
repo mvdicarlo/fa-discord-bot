efc0d0f17491ea559b6fea8ceffe41fde21fcf4b
Michael DiCarlo <michae.v.dicarlo@gmail.com>
2018-09-07 09:29:51 -0400

    Initialize project

---
+++ index.js
@@ -0,0 +1,66 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const handler = require('./src/command-handler');
+const commandIndicator = process.env.indicator || 'owo';
+const logger = require('winston');
+
+logger.configure({
+    transports: [new(logger.transports.Console)({
+        colorize: true
+    })]
+});
+
+function isBotCommand(msg) {
+    if (msg.includes('owo ') || msg.includes(' owo')) {
+        return false;
+    }
+
+    return msg && msg.substring(0, commandIndicator.length) === commandIndicator && msg.length > commandIndicator.length;
+}
+
+function getCommand(msg) {
+    return msg.substring(commandIndicator.length).split(' ');
+}
+
+function getCommandsList() {
+    return 'FAB Commands:\n```' + `${commandIndicator}random <username>` + '```';
+}
+
+client.on('ready', (evt) => {
+    logger.info('Connected');
+    logger.info(`Logged in as: ${client.user.tag}`);
+
+    client.user.setStatus('available')
+    client.user.setPresence({
+        game: {
+            name: `to ${commandIndicator}help`,
+            type: 'Listening'
+        }
+    });
+});
+
+client.on('message', (msg) => {
+    const message = msg.content.trim();
+    if (!isBotCommand(message))
+        return;
+
+    const cmd = getCommand(message);
+    switch (cmd[0].toLowerCase()) {
+        case 'help':
+            msg.author.sendMessage(getCommandsList());
+            break;
+        case 'random':
+            if (cmd.length > 1) {
+                handler.randomImage(cmd[1], (err, res) => {
+                    msg.reply(err ? err : res);
+                });
+            } else {
+                msg.reply('Please provide a username.');
+            }
+            break;
+        default:
+            msg.reply(`Unknown command: type ${commandIndicator}help for a list of commands`);
+    }
+});
+
+client.login(process.env.fab_token);
---
+++ src/command-handler.js
@@ -0,0 +1,85 @@
+const request = require('request');
+const FileCookieStore = require('tough-cookie-filestore');
+const cheerio = require('cheerio');
+const logger = require('winston');
+
+const cookieStore = new FileCookieStore('cookies.json');
+const jar = request.jar(cookieStore);
+
+request.defaults({
+    jar
+});
+
+function checkStatus() {
+    request.get({
+        url: 'http://www.furaffinity.net',
+        jar
+    }, (err, res, body) => {
+        if (err) {
+            logger.log('error', 'Cookies are invalid', err);
+        } else {
+            if (body.includes('Log Out')) {
+                logger.info('Cookies still valid.');
+            } else {
+                logger.error('Unable to verify cookies.');
+            }
+        }
+    });
+}
+
+setInterval(checkStatus, 60 * 60000);
+checkStatus();
+
+function getPage(url) {
+    return new Promise((resolve, reject) => {
+        request.get({
+            url,
+            jar
+        }, (err, res, body) => {
+            if (err) {
+                reject(false);
+            } else {
+                resolve(body);
+            }
+        });
+    })
+}
+
+module.exports = {
+    randomImage(username, cb) {
+        request.get({
+            url: `http://www.furaffinity.net/user/${username}/`,
+            jar
+        }, (err, res, body) => {
+            if (err) {
+                cb(`Unable to retrieve user gallery for ${username}`, null);
+            } else {
+                const $ = cheerio.load(body);
+                const submissionsCount = $('td').eq(16).text().split('\n')[2].trim().split(' ')[1].trim();
+                const pageCount = Number(submissionsCount) / 48;
+                const promises = [];
+                for (let i = 0; i < pageCount; i++) {
+                    promises.push(getPage(`http://www.furaffinity.net/gallery/${username}/${i}/`));
+                }
+
+                Promise.all(promises).catch(err => {
+                    cb(`Unable to retrieve user gallery for ${username}`, null);
+                }).then(pages => {
+                    const links = [];
+                    const images = pages.map(p => {
+                        const $ = cheerio.load(p);
+                        $('a').each((im, el) => {
+                            if (el.name === 'a' && el.attribs.href && el.attribs.href.includes('/view/')) {
+                                links.push(el.attribs.href);
+                            }
+                        });
+                    });
+
+                    const link = 'http://www.furaffinity.net' + links[Math.floor(Math.random() * links.length)];
+
+                    cb(null, link);
+                });
+            }
+        });
+    }
+}
